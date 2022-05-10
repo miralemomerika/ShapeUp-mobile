f562c8c4ad9b13d3486367f33e1abf3dbb342753
Neira Omerika <neira.omerika@edu.fit.ba>
2022-05-10 19:28:48 +0200

    final design changes

--- shapeup_mobile/lib/pages/Profil.dart
+++ shapeup_mobile/lib/pages/Profil.dart
@@ -10,7 +10,6 @@ class Profil extends StatefulWidget {
 }
 
 class _Profil extends State<Profil> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,109 +26,123 @@ class _Profil extends State<Profil> {
     );
   }
 
-  Widget user () {
+  Widget user() {
     return FutureBuilder<User>(
-      future: GetUser(),
-      builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
-        if(snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator(),);
-        }
-        else {
-          if(snapshot.hasError) {
-            return Center(child: Text('${snapshot.error}'),);
-          }
-          else {
-            return Container(
-              height: 200,
-              child: Container(
-                child: usersProfile(snapshot.data)
-              ),
+        future: GetUser(),
+        builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
             );
+          } else {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text('${snapshot.error}'),
+              );
+            } else {
+              return Container(
+                height: 200,
+                child: Container(child: usersProfile(snapshot.data)),
+              );
+            }
           }
-        }
-      }
-    );
+        });
   }
 
-  Widget usersProfile (user) {
+  Widget usersProfile(user) {
     return Container(
-      margin: EdgeInsets.all(5),
-      child: Column(
-        children: [ 
-          SizedBox(
-            height: 150,
-            child: Container(
-              alignment: Alignment.center,
-              child: ClipOval(
-                child: imageShow(user)
+        margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 200,
+              width: 200,
+              child: Container(
+                alignment: Alignment.center,
+                child: ClipOval(child: imageShow(user)),
               ),
             ),
-          ),
-          SizedBox(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Container(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        _prefixIcon(Icons.people),
-                        Container(
-
-                          alignment: Alignment.centerLeft,
-                          child: Text("Ime", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold,)),
+            SizedBox(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                      alignment: Alignment.centerLeft,
+                      child: Padding(
+                        padding: EdgeInsets.fromLTRB(25, 0, 0, 20),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                                alignment: Alignment.centerLeft,
+                                child: Row(
+                                  children: [
+                                    _prefixIcon(Icons.people),
+                                    Text("Ime i prezime",
+                                        style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                        )),
+                                  ],
+                                )),
+                            Container(
+                              margin: EdgeInsets.fromLTRB(25, 0, 0, 20),
+                              alignment: Alignment.centerLeft,
+                              child: Text(user.firstName + " " + user.lastName,
+                                  style: TextStyle(fontSize: 18)),
+                            ),
+                            Container(
+                                alignment: Alignment.centerLeft,
+                                child: Row(
+                                  children: [
+                                    _prefixIcon(Icons.phone),
+                                    Text(
+                                      "Mobitel",
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                )),
+                            Container(
+                              margin: EdgeInsets.fromLTRB(25, 0, 0, 20),
+                              alignment: Alignment.centerLeft,
+                              child: Text(user.phoneNumber,
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                  )),
+                            ),
+                            Container(
+                              alignment: Alignment.centerLeft,
+                              child: Row(
+                                  children: [
+                                    _prefixIcon(Icons.email),
+                                    Text(
+                                      "Email",
+                                      style: TextStyle(
+                                          fontSize: 20, fontWeight: FontWeight.bold)
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            Container(
+                              margin: EdgeInsets.fromLTRB(25, 0, 0, 0),
+                              alignment: Alignment.centerLeft,
+                              child: Text(user.email,
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                  )),
+                            ),
+                          ],
                         ),
-                        Container(
-                          margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
-                          alignment: Alignment.centerLeft,
-                          child: Text(user.firstName, style: TextStyle(fontSize: 18)),
-                        ),
-
-                        Container(
-                          margin: EdgeInsets.fromLTRB(0, 0, 0, 5),
-                          alignment: Alignment.centerLeft,
-                          child: Text("Prezime", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                        ),
-                        Container(
-
-                          alignment: Alignment.centerLeft,
-                          child: Text(user.lastName, style: TextStyle(fontSize: 18,),),
-                        ),
-                        _prefixIcon(Icons.phone),
-                        Container(
-
-                          alignment: Alignment.centerLeft,
-                          child: Text("Mobitel", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                        ),
-                        Container(
-                          margin: EdgeInsets.fromLTRB(0, 5, 0, 0 ),
-                          alignment: Alignment.centerLeft,
-                          child: Text(user.phoneNumber, style: TextStyle(fontSize: 20,)),
-                        ),
-                        _prefixIcon(Icons.email),
-                        Container(
-                          alignment: Alignment.centerLeft,
-                          child: Text("Email", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-                        ),
-                        Container(
-                          margin: EdgeInsets.fromLTRB(0, 5, 0, 0 ),
-                          alignment: Alignment.centerLeft,
-                          child: Text(user.email, style: TextStyle(fontSize: 20,)),
-                        ),
-                      ],
-                    ),
-                  )
-                )
-              ],
-            ),
-          )
-        ],
-      )
-    );
+                      ))
+                ],
+              ),
+            )
+          ],
+        ));
   }
+
   _prefixIcon(IconData iconData) {
     return ConstrainedBox(
       constraints: const BoxConstraints(minWidth: 48.0, minHeight: 48.0),
@@ -137,7 +150,6 @@ class _Profil extends State<Profil> {
           alignment: Alignment.centerLeft,
           padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
           margin: const EdgeInsets.only(right: 8.0),
-
           child: Icon(
             iconData,
             size: 30,
@@ -146,16 +158,26 @@ class _Profil extends State<Profil> {
     );
   }
 
-  Future<User> GetUser () async {
+  Future<User> GetUser() async {
     var user = await HttpService.GetObj('Users/profile-info', null);
 
     return User.fromJson(user);
   }
 
-  Widget imageShow (user) {
-    if(user.slika != null && user.slika!.isNotEmpty){
-      return Image(image: MemoryImage(user.slika!), fit: BoxFit.cover, width: 125, height: 125,);
+  Widget imageShow(user) {
+    if (user.slika != null && user.slika!.isNotEmpty) {
+      return Image(
+        image: MemoryImage(user.slika!),
+        fit: BoxFit.cover,
+        width: 150,
+        height: 150,
+      );
     }
-    return Image(image: AssetImage('assets/no-image.png'), fit: BoxFit.cover, width: 125, height: 125,);
+    return Image(
+      image: AssetImage('assets/no-image.png'),
+      fit: BoxFit.cover,
+      width: 150,
+      height: 150,
+    );
   }
 }
